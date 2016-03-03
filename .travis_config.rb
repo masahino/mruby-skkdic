2182a3fe61198753daccfef90eaea5f66d21c83b
masahino <masahino@gmail.com>
2016-03-03 22:10:17 +0900

    change config

--- .travis_config.rb
+++ .travis_config.rb
@@ -2,5 +2,8 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   enable_test
   conf.gembox 'default'
+  conf.gem :github => 'mattn/mruby-iconv' do |g|
+    g.linker.libraries.delete 'iconv'
+  end
   conf.gem '#{MRUBY_ROOT}/..'
 end
